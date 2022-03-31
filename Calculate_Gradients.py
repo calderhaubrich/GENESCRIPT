e9e3a5c29211b3d9323f890fb0ca649d15196723
calderhaubrich <98188741+calderhaubrich@users.noreply.github.com>
2022-03-31 11:10:06 -0600

    Update Calculate_Gradients.py

--- Calculate_Gradients.py
+++ Calculate_Gradients.py
@@ -3,6 +3,7 @@
 
 import cheasefiles
 import millergeometryfunction
+import rho_tor_to_miller_r as miller
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
@@ -20,6 +21,8 @@ proffpath = '/home/calderhaubrich/GENE/DIIID_163241_profiles/p163241.03500_e5059
 def main(args):
     parser = argparse.ArgumentParser(description='Calculates gradients at given rhotor position.')
     parser.add_argument('file', help='input some gfile')
+    parser.add_argument('-r','--rho', default=False, action='store_true', help='Finds rho gradients')
+    parser.add_argument('-m','--miller',default=False, action='store_true', help='Plots with miller r')
     parser.add_argument('location', help='rhotor location')
     parser.add_argument('-a','--all', default=False, action='store_true', help='Plot species with respect to rhotor')
     args = parser.parse_args(args)
@@ -27,19 +30,136 @@ def main(args):
     efitfpath = args.file
     rho_tr = float(args.location)
 
+    if (args.rho):
+        geomdata = millergeometryfunction.finder(efitfpath,rho_tr)
+        R0 = geomdata[0]
+        a  = geomdata[1]
+        drho_tor_dr = geomdata[2]
 
-    geomdata = millergeometryfunction.finder(efitfpath,rho_tr)
-    R0 = geomdata[0]
-    a  = geomdata[1]
-    drho_tor_dr = geomdata[2]
+        setparam = {'nrhomesh':'rhotor'}
 
+        efitprof = cheasefiles.read_eqdsk(eqdskfpath=efitfpath,setParam=setparam)
+        specprof = cheasefiles.read_profiles(profilesfpath=proffpath,setParam=setparam,eqdsk=efitfpath)
+
+
+        rhotor= np.array(specprof['rhotor'])
+
+        Te= np.array(specprof['Te'])
+        Ti= np.array(specprof['Ti'])
+        ne= np.array(specprof['ne'])
+        ni= np.array(specprof['ni'])
+        nb= np.array(specprof['nb'])
+        nc= np.array(specprof['nz'])
+        Pb= np.array(specprof['Pb'])
+        Vtor= np.array(specprof['Vtor'])
+        Vpol= np.array(specprof['Vpol'])
+        Ptot= np.array(specprof['pressure'])
+        q= np.array(efitprof['q'])
+        Tb= (Pb/nb)*(1/(1.6*10e-19))
+
+        interpol_order = 3
+
+        ne_spl = InterpolatedUnivariateSpline(rhotor, ne, k=interpol_order)
+        ni_spl = InterpolatedUnivariateSpline(rhotor, ni, k=interpol_order)
+        nb_spl = InterpolatedUnivariateSpline(rhotor, nb, k=interpol_order)
+        te_spl = InterpolatedUnivariateSpline(rhotor, Te, k=interpol_order)
+        ti_spl = InterpolatedUnivariateSpline(rhotor, Ti, k=interpol_order)
+        tb_spl = InterpolatedUnivariateSpline(rhotor, Tb, k=interpol_order)
+        pb_spl = InterpolatedUnivariateSpline(rhotor, Pb, k=interpol_order)
+        vtor_spl = InterpolatedUnivariateSpline(rhotor, Vtor, k=interpol_order)
+        Ptot_spl = InterpolatedUnivariateSpline(rhotor, Ptot, k=interpol_order)
+
+        dnedrho_spl = ne_spl.derivative()
+        dnidrho_spl = ni_spl.derivative()
+        dnbdrho_spl = nb_spl.derivative()
+        dtedrho_spl = te_spl.derivative()
+        dtidrho_spl = ti_spl.derivative()
+        dtbdrho_spl = tb_spl.derivative()
+        dpbdrho_spl = pb_spl.derivative()
+        dvtordrho_spl = vtor_spl.derivative()   
+
+        dnedrho = dnedrho_spl(rhotor)
+        dnidrho = dnidrho_spl(rhotor)
+        dnbdrho = dnbdrho_spl(rhotor)
+        dtedrho = dtedrho_spl(rhotor)
+        dtidrho = dtidrho_spl(rhotor)
+        dtbdrho = dtbdrho_spl(rhotor)
+        dpbdrho = dpbdrho_spl(rhotor)
+        dvtordrho = dvtordrho_spl(rhotor)
+        #domegarho = domegdrho_spl(rhotor)
+
+        #dvpolrho = dvpoldrho_spl(rhotor)
+
+
+        # Normalized paramters at target toroidal radial location
+        Z0= 0.0031727/R0
+        mD= 3.343583719e-27         # Mass of deuterium in kg
+        m_ref= mD                   # Deuteriam mass as reference mass
+        mD_tr= mD/m_ref            # Normalized deuterium mass
+        me= 9.10938356e-31          # Mass of electron in SI Units (kg)
+        me_tr= me/m_ref             # Normalized electron mass
+        #n_ref= ne_spl(rhotor)      # reference temparture as function of rhotor
+        #t_ref= ti_spl(rhotor) 
+        nref_tr= ne_spl(rho_tr)     # Electron density as refernce density at target location
+        tref_tr= ti_spl(rho_tr)     # main ion tempeartrure as reference temperature at target location
+
+        vtor_tr= vtor_spl(rho_tr)     # toroidal rotation velocity m/s at reference location
+        #omeg_tr= omeg_spl(rho_tr)    # toroidal rotation frequency in rad/sec at reference location
+
+        ne_tr= ne_spl(rho_tr)/nref_tr
+        ni_tr= ni_spl(rho_tr)/nref_tr
+        nb_tr= nb_spl(rho_tr)/nref_tr
+
+        te_tr= te_spl(rho_tr)/tref_tr
+        ti_tr= ti_spl(rho_tr)/tref_tr
+        tb_tr= tb_spl(rho_tr)/tref_tr
+
+
+        #Calculate Gradients with respect to major radius R0
+        omne_tr= dnedrho_spl(rho_tr)*[-R0/ne_spl(rho_tr)]*drho_tor_dr #omne_tr = -(Lref/n)(dn/drho)
+        omni_tr= dnidrho_spl(rho_tr)*[-R0/ni_spl(rho_tr)]*drho_tor_dr
+        omnb_tr= dnbdrho_spl(rho_tr)*[-R0/nb_spl(rho_tr)]*drho_tor_dr
+        omte_tr= dtedrho_spl(rho_tr)*[-R0/te_spl(rho_tr)]*drho_tor_dr
+        omti_tr= dtidrho_spl(rho_tr)*[-R0/ti_spl(rho_tr)]*drho_tor_dr
+        omtb_tr= dtbdrho_spl(rho_tr)*[-R0/tb_spl(rho_tr)]*drho_tor_dr
+
+        # To calculate ion density when no Carbon impurity present
+        #nc_tr= 0
+        #omnc_tr=0
+        #ni_tr=(ne_tr-6*nc_tr);
+        #omni_tr= (ne_tr*omne_tr- 6*nc_tr*omnc_tr)/ni_tr;
+
+        # Carboon density profile from the quasineutrality condition
+        nc_tr=(ne_tr - ni_tr)/6
+        omnc_tr =(ne_tr*omne_tr -ni_tr*omni_tr)/(6*nc_tr)
+
+        #Calculate Gradients with respect to minor radius a
+        omne = dnedrho_spl(rho_tr)*[-a/ne_spl(rho_tr)]*drho_tor_dr
+        omni = dnidrho_spl(rho_tr)*[-a/ni_spl(rho_tr)]*drho_tor_dr
+        omnb = dnbdrho_spl(rho_tr)*[-a/nb_spl(rho_tr)]*drho_tor_dr
+        omte = dtedrho_spl(rho_tr)*[-a/te_spl(rho_tr)]*drho_tor_dr
+        omti = dtidrho_spl(rho_tr)*[-a/ti_spl(rho_tr)]*drho_tor_dr
+        omtb = dtbdrho_spl(rho_tr)*[-a/tb_spl(rho_tr)]*drho_tor_dr
+
+        print('Location   = {}'.format(rho_tr))
+
+        print('omne_tr    = {}'.format(omne_tr))
+        print('omni_tr    = {}'.format(omni_tr))
+        print('omnb_tr    = {}'.format(omnb_tr))
+        print('omte_tr    = {}'.format(omte_tr))
+        print('omti_tr    = {}'.format(omti_tr))
+        print('omtb_tr    = {}'.format(omtb_tr))
+
+    if (args.miller):
+        mill = miller.findmiller(efitfpath)
+        rad = mill[0]
     setparam = {'nrhomesh':'rhotor'}
 
     efitprof = cheasefiles.read_eqdsk(eqdskfpath=efitfpath,setParam=setparam)
     specprof = cheasefiles.read_profiles(profilesfpath=proffpath,setParam=setparam,eqdsk=efitfpath)
+    rhopsi = np.array(specprof['rhopsi'])
+    rhotor = np.array(specprof['rhotor'])
 
-
-    rhotor= np.array(specprof['rhotor'])
 
     Te= np.array(specprof['Te'])
     Ti= np.array(specprof['Ti'])
@@ -52,100 +172,15 @@ def main(args):
     Vpol= np.array(specprof['Vpol'])
     Ptot= np.array(specprof['pressure'])
     q= np.array(efitprof['q'])
-    Tb= (Pb/nb)*(1/(1.6*10e-19))
 
-    interpol_order = 3
-
-    ne_spl = InterpolatedUnivariateSpline(rhotor, ne, k=interpol_order)
-    ni_spl = InterpolatedUnivariateSpline(rhotor, ni, k=interpol_order)
-    nb_spl = InterpolatedUnivariateSpline(rhotor, nb, k=interpol_order)
-    te_spl = InterpolatedUnivariateSpline(rhotor, Te, k=interpol_order)
-    ti_spl = InterpolatedUnivariateSpline(rhotor, Ti, k=interpol_order)
-    tb_spl = InterpolatedUnivariateSpline(rhotor, Tb, k=interpol_order)
-    pb_spl = InterpolatedUnivariateSpline(rhotor, Pb, k=interpol_order)
-    vtor_spl = InterpolatedUnivariateSpline(rhotor, Vtor, k=interpol_order)
-    Ptot_spl = InterpolatedUnivariateSpline(rhotor, Ptot, k=interpol_order)
-
-    dnedrho_spl = ne_spl.derivative()
-    dnidrho_spl = ni_spl.derivative()
-    dnbdrho_spl = nb_spl.derivative()
-    dtedrho_spl = te_spl.derivative()
-    dtidrho_spl = ti_spl.derivative()
-    dtbdrho_spl = tb_spl.derivative()
-    dpbdrho_spl = pb_spl.derivative()
-    dvtordrho_spl = vtor_spl.derivative()   
-
-    dnedrho = dnedrho_spl(rhotor)
-    dnidrho = dnidrho_spl(rhotor)
-    dnbdrho = dnbdrho_spl(rhotor)
-    dtedrho = dtedrho_spl(rhotor)
-    dtidrho = dtidrho_spl(rhotor)
-    dtbdrho = dtbdrho_spl(rhotor)
-    dpbdrho = dpbdrho_spl(rhotor)
-    dvtordrho = dvtordrho_spl(rhotor)
-    #domegarho = domegdrho_spl(rhotor)
-
-    #dvpolrho = dvpoldrho_spl(rhotor)
-
-
-    # Normalized paramters at target toroidal radial location
-    Z0= 0.0031727/R0
-    mD= 3.343583719e-27         # Mass of deuterium in kg
-    m_ref= mD                   # Deuteriam mass as reference mass
-    mD_tr= mD/m_ref            # Normalized deuterium mass
-    me= 9.10938356e-31          # Mass of electron in SI Units (kg)
-    me_tr= me/m_ref             # Normalized electron mass
-    #n_ref= ne_spl(rhotor)      # reference temparture as function of rhotor
-    #t_ref= ti_spl(rhotor) 
-    nref_tr= ne_spl(rho_tr)     # Electron density as refernce density at target location
-    tref_tr= ti_spl(rho_tr)     # main ion tempeartrure as reference temperature at target location
-
-    vtor_tr= vtor_spl(rho_tr)     # toroidal rotation velocity m/s at reference location
-    #omeg_tr= omeg_spl(rho_tr)    # toroidal rotation frequency in rad/sec at reference location
-
-    ne_tr= ne_spl(rho_tr)/nref_tr
-    ni_tr= ni_spl(rho_tr)/nref_tr
-    nb_tr= nb_spl(rho_tr)/nref_tr
-
-    te_tr= te_spl(rho_tr)/tref_tr
-    ti_tr= ti_spl(rho_tr)/tref_tr
-    tb_tr= tb_spl(rho_tr)/tref_tr
-
-
-    #Calculate Gradients with respect to major radius R0
-    omne_tr= dnedrho_spl(rho_tr)*[-R0/ne_spl(rho_tr)]*drho_tor_dr #omne_tr = -(Lref/n)(dn/drho)
-    omni_tr= dnidrho_spl(rho_tr)*[-R0/ni_spl(rho_tr)]*drho_tor_dr
-    omnb_tr= dnbdrho_spl(rho_tr)*[-R0/nb_spl(rho_tr)]*drho_tor_dr
-    omte_tr= dtedrho_spl(rho_tr)*[-R0/te_spl(rho_tr)]*drho_tor_dr
-    omti_tr= dtidrho_spl(rho_tr)*[-R0/ti_spl(rho_tr)]*drho_tor_dr
-    omtb_tr= dtbdrho_spl(rho_tr)*[-R0/tb_spl(rho_tr)]*drho_tor_dr
-
-    # To calculate ion density when no Carbon impurity present
-    #nc_tr= 0
-    #omnc_tr=0
-    #ni_tr=(ne_tr-6*nc_tr);
-    #omni_tr= (ne_tr*omne_tr- 6*nc_tr*omnc_tr)/ni_tr;
-
-    # Carboon density profile from the quasineutrality condition
-    nc_tr=(ne_tr - ni_tr)/6
-    omnc_tr =(ne_tr*omne_tr -ni_tr*omni_tr)/(6*nc_tr)
-
-    #Calculate Gradients with respect to minor radius a
-    omne = dnedrho_spl(rho_tr)*[-a/ne_spl(rho_tr)]*drho_tor_dr
-    omni = dnidrho_spl(rho_tr)*[-a/ni_spl(rho_tr)]*drho_tor_dr
-    omnb = dnbdrho_spl(rho_tr)*[-a/nb_spl(rho_tr)]*drho_tor_dr
-    omte = dtedrho_spl(rho_tr)*[-a/te_spl(rho_tr)]*drho_tor_dr
-    omti = dtidrho_spl(rho_tr)*[-a/ti_spl(rho_tr)]*drho_tor_dr
-    omtb = dtbdrho_spl(rho_tr)*[-a/tb_spl(rho_tr)]*drho_tor_dr
-
-    print('Location   = {}'.format(rho_tr))
-
-    print('omne_tr    = {}'.format(omne_tr))
-    print('omni_tr    = {}'.format(omni_tr))
-    print('omnb_tr    = {}'.format(omnb_tr))
-    print('omte_tr    = {}'.format(omte_tr))
-    print('omti_tr    = {}'.format(omti_tr))
-    print('omtb_tr    = {}'.format(omtb_tr))
+    plt.figure()
+    #plt.plot(rhotor,ne)
+    #plt.plot(rad,ne)
+    plt.plot(rad,rhotor, linewidth=3.0)
+    plt.ylabel('r/a', fontsize=16)
+    plt.xlabel('$\\rho_{tor}$', fontsize=16)
+    plt.grid()
+    plt.show()
 
     if (args.all):
         # Ion temperature plot
